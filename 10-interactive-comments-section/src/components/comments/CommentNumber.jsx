c639947b8f852fe1e72c5e777431ac91b8b76a64
StevenLiu <stevenliu986@gmail.com>
2023-02-18 12:27:44 +1100

    modified:   10-interactive-comments-section/src/components/comments/CommentNumber.jsx

--- 10-interactive-comments-section/src/components/comments/CommentNumber.jsx
+++ 10-interactive-comments-section/src/components/comments/CommentNumber.jsx
@@ -2,7 +2,7 @@ import style from "./CommentNumber.module.css";
 
 const CommentNumber = () => {
   return (
-    <div>
+    <div className={style.box}>
       <p className={style.symbol}>+</p>
       <p className={style.count}>12</p>
       <p className={style.symbol}>-</p>
