47288969a780039f4ca85264a899e3f1d52d2427
StevenLiu <stevenliu986@gmail.com>
2023-02-18 12:59:23 +1100

    modified:   10-interactive-comments-section/src/components/comments/CommentContent.jsx

--- 10-interactive-comments-section/src/components/comments/CommentContent.jsx
+++ 10-interactive-comments-section/src/components/comments/CommentContent.jsx
@@ -1,5 +1,20 @@
+import amyrobson from "../../assets/images/avatars/image-amyrobson.png";
+import style from "./CommentContent.module.css";
+
 const CommentContent = () => {
-  return <div>CommentContent</div>;
+  return (
+    <div className={style.content}>
+      <img src={amyrobson} alt="avatar-amyrobson" />
+      <span>amyrobson</span>
+      <span>reply</span>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam illum
+        voluptates praesentium enim nostrum quis obcaecati maxime voluptatibus
+        fugiat nihil molestiae accusamus, fugit commodi exercitationem eum.
+        Libero nulla ut explicabo!
+      </p>
+    </div>
+  );
 };
 
 export default CommentContent;
