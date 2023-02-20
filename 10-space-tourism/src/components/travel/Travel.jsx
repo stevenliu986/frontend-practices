906cfffd138f6fe0383a858b611b9de8c3ec1237
StevenLiu <stevenliu986@gmail.com>
2023-02-20 20:34:29 +1100

    modified:   10-space-tourism/src/components/travel/Travel.jsx

--- 10-space-tourism/src/components/travel/Travel.jsx
+++ 10-space-tourism/src/components/travel/Travel.jsx
@@ -1,21 +1,34 @@
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  width: 450px;
+  height: 250px;
+`;
+
+const Emphasize = styled.div`
+  font-size: 48px;
+`;
+
 const DetailContent = styled.div`
-  color: ;
+  color: grey;
+`;
+
+const CapWord = styled.div`
+  text-transform: uppercase;
 `;
 
 const Travel = () => {
   return (
-    <div>
-      <p>So, you want to travel to</p>
-      <p>Space</p>
-      <p>
+    <Wrapper>
+      <CapWord>So, you want to travel to</CapWord>
+      <Emphasize>Space</Emphasize>
+      <DetailContent>
         Let’s face it; if you want to go to space, you might as well genuinely
         go to outer space and not hover kind of on the edge of it. Well sit
         back, and relax because we’ll give you a truly out of this world
         experience!
-      </p>
-    </div>
+      </DetailContent>
+    </Wrapper>
   );
 };
 
