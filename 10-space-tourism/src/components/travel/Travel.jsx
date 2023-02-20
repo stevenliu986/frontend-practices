021f195255534533e5948a6700c510955e16dda8
StevenLiu <stevenliu986@gmail.com>
2023-02-20 20:20:06 +1100

    modified:   10-space-tourism/src/components/travel/Travel.jsx

--- 10-space-tourism/src/components/travel/Travel.jsx
+++ 10-space-tourism/src/components/travel/Travel.jsx
@@ -1,5 +1,16 @@
 const Travel = () => {
-  return <div>Travel Description</div>;
+  return (
+    <div>
+      <p>So, you want to travel to</p>
+      <p>Space</p>
+      <p>
+        Let’s face it; if you want to go to space, you might as well genuinely
+        go to outer space and not hover kind of on the edge of it. Well sit
+        back, and relax because we’ll give you a truly out of this world
+        experience!
+      </p>
+    </div>
+  );
 };
 
 export default Travel;
